217225362642a770bdf35316176fe59fd999a08e
Kabugi Guandaru <117659945+Guandaru-Kabugi@users.noreply.github.com>
2024-12-13 15:53:57 +0300

    Update test_mailgun.py

--- test_mailgun.py
+++ test_mailgun.py
@@ -8,22 +8,6 @@ from django.conf import settings
 
 
 """
-def send_simple_message():
-    response = requests.post(
-        "https://api.mailgun.net/v3/sandbox51922198327d4f83a64875a9e212637b.mailgun.org/messages",
-        auth=("api", "6080df50948b0a8aa615077b4aff5293-da554c25-9e1ad0f8"),
-        data={
-            "from": "Excited User <mailgun@sandbox51922198327d4f83a64875a9e212637b.mailgun.org>",
-            "to": ["guandarualex3@gmail.com", "sandbox51922198327d4f83a64875a9e212637b.mailgun.org"],
-            "subject": "Hello",
-            "text": "Testing some Mailgun awesomeness!",
-        },
-    )
-
-    print(f"Status Code: {response.status_code}")
-    print(f"Response: {response.json()}")
-
-send_simple_message()
 """
 def send_email():
     # Hardcoded email addresses for testing
